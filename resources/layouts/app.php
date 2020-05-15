aab746b5d7278b896a722151b971b7e240d7197b
Arthaud Proust <arthaudp33@gmail.com>
2020-05-16 00:52:43 +0200

    initial commit

--- resources/layouts/app.php
+++ resources/layouts/app.php
@@ -10,7 +10,7 @@
 
     <script src="/public/js/jquery.js"></script>
     <script src="/public/js/bootstrap.bundle.min.js"></script>
-    <script src="/public/js/Atom.js"></script>
+    <script src="/public/js/atom.js"></script>
     <script src="/public/js/meta.js"></script>
     <link rel="stylesheet" href="/public/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/public/css/master.css"/>
