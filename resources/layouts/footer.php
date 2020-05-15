f776e94944573ecc13c847d9bb433e4f33fbcd18
Arthaud Proust <arthaudp33@gmail.com>
2020-05-16 00:55:22 +0200

    initial commit

--- resources/layouts/footer.php
+++ resources/layouts/footer.php
@@ -11,8 +11,8 @@
         <div class="col-6 col-md">
             <h5>À propos</h5>
             <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="https://arthaud.dev">Développé par Arthaud</a></li>
-                <li><a class="text-muted" href="https://github.com/arthaud-proust">Code source</a></li>
+                <li><a class="text-muted" href="https://github.com/arthaud-proust/">Développé par Arthaud</a></li>
+                <li><a class="text-muted" href="https://github.com/arthaud-proust/physics-tools">Code source</a></li>
             </ul>
         </div>
     </div>
